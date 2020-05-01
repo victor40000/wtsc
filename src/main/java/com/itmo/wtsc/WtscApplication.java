7a80e1e82d4283baa8bfc3b3c69d85b95db8160d
Viktor <victor40000@yandex.ru>
2020-05-01 14:52:39 +0300

    initial

---
+++ src/main/java/com/itmo/wtsc/WtscApplication.java
@@ -0,0 +1,13 @@
+package com.itmo.wtsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itmo/wtsc/WtscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmo.wtsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
